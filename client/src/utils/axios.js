c503f4ab31d3c4549026ef1132c80d7e15d36034
weslife911 <mfonfubikoma.com>
2025-05-07 20:06:00 +0100

    Added start command

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://the-cure-foundation.onrender.com/api",
     withCredentials: true
 });
