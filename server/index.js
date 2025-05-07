d43b02a00b54ba20b2950e0600b5566c3b91f1f4
weslife911 <mfonfubikoma.com>
2025-05-07 20:53:08 +0100

    Hosted client on vercel

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://the-cure-foundation.vercel.app/",
     credentials: true
 }));
 app.use(cookieParser());
