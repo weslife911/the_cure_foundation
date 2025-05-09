af0fffed9550d0f48c12f5d27835c7869ae4f1a2
weslife911 <mfonfubikoma.com>
2025-05-09 10:03:20 +0100

    Testing

--- client/src/components/QuestionBox.jsx
+++ client/src/components/QuestionBox.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function QuestionBox() {
+function QuestionBox({ question }) {
   return (
-    <div>
-      
-    </div>
+    <li style={{background: "#ffffff",padding: "15px 20px",margin: "10px 0",borderRadius: "8px",display: "flex",justifyContent: "space-between",alignItems: "center",boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",transition: "all 0.3s ease-in-out"}}>
+                    <span className="file-name" style={{fontSize: "16px",fontWeight: "500",color: "#555"}}>
+                      {question.fileName}
+                    </span>
+                    <Link to={question.fileLink} className="download-btn" download style={{padding: "10px 18px",fontSize: "14px",color: "white",backgroundColor: "#007bff",textDecoration: "none",borderRadius: "6px",transition: "background 0.3s"}}>Download</Link>
+                </li>
   )
 }
 
--- client/src/pages/verified/NotesPage.jsx
+++ client/src/pages/verified/NotesPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom";
+import { getAuthUser } from '../../features/users/userSlice';
+import { getAllQuestions } from '../../features/questions/questionSlice';
 
 function NotesPage() {
+
+  const authUser = useSelector(getAuthUser);
+  
+    const questions = useSelector(getAllQuestions).filter(question => question.fileGenre === "ca" &&  question.field === authUser.fieldOfStudy);
+
   return (
     <div id="ca" className="page wb-page">
                                     <style id="sectionStyle-10946059">
@@ -35,12 +43,9 @@ function NotesPage() {
             <ul className="file-list" style={{listStyle: "none",padding: 0}}>
                     
                 
-                <li style={{background: "#ffffff",padding: "15px 20px",margin: "10px 0",borderRadius: "8px",display: "flex",justifyContent: "space-between",alignItems: "center",boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",transition: "all 0.3s ease-in-out"}}>
-                    <span className="file-name" style={{fontSize: "16px",fontWeight: "500",color: "#555"}}>
-                      {/* file name */}
-                    </span>
-                    <Link to className="download-btn" download style={{padding: "10px 18px",fontSize: "14px",color: "white",backgroundColor: "#007bff",textDecoration: "none",borderRadius: "6px",transition: "background 0.3s"}}>Download</Link>
-                </li>
+            {questions.map((question) => (
+                  <QuestionBox key={question._id} question={question} />
+                ))}
 
 
                 
