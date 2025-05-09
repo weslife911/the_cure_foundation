06a989ba21303af89ba2e4113393870bc341a9f1
weslife911 <mfonfubikoma.com>
2025-05-09 12:05:10 +0100

    Testing

--- server/index.js
+++ server/index.js
@@ -45,10 +45,6 @@ const PORT = Number(process.env.PORT) || 8080;
 
 connectToDB();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 })
-
-// app.listen(PORT, "0.0.0.0", () => {
-//   console.log(`Server is running on port ${PORT}`);
-// })
